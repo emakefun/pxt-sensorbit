10579d7906890302aa5e187d841b0b8de6deeff6
joey@emakefun.com <joey@emakefun.com>
2020-07-26 20:49:10 +0800

    add act

--- main.ts
+++ main.ts
@@ -101,6 +101,14 @@ enum _selectcolor{
 	_green=2,
 }
 
+enum run_turn{
+
+    //% block="forward"
+    forward = 0,
+    //% block="reverse"
+    reverse = 1,
+}
+
 const LED0_ON_L = 0x06
 const LED0_ON_H = 0x07
 const LED0_OFF_L = 0x08
@@ -112,6 +120,432 @@ const ALL_LED_OFF_H = 0xFD
 
 //% color="#FFA500" weight=10 icon="\uf2c9" block="Sensor:bit"
 namespace sensors {
+
+	/**
+     * 无源蜂鸣器
+     */
+    
+    //% blockId=actuator_buzzer0 block="actuator_buzzer0 pin ：%pin|status %status"   group="无源蜂鸣器"
+	//% weight=70
+	//% subcategory="执行器"
+    export function actuator_buzzer0(pin: DigitalPin,status: on_off): void {
+        pins.digitalWritePin(pin,status)
+    }
+  
+	/**
+	 * 有源蜂鸣器
+	 */
+    //% blockId=actuator_buzzer1 block="actuator_buzzer1 pin ：%pin|status %status"   group="有源蜂鸣器"
+	//% weight=70
+	//% subcategory="执行器"
+    export function actuator_buzzer1(pin: AnalogPin,freq: number): void {
+        pins.analogWritePin(pin,freq)
+    }
+	   	
+	/**
+	 * 继电器
+	 */
+    //% blockId=actuator_relay block="actuator_relay pin ：%pin|status %status"   group="继电器"
+	//% weight=70
+	//% subcategory="执行器"
+    export function actuator_relay(pin: DigitalPin,status: on_off): void {
+        pins.digitalWritePin(pin,status)
+    }
+		   	
+	/**
+	 * 直流电机
+	 */
+	//% blockId=actuator_motor_run block="actuator_motor_run  %turn"  group="直流电机"
+    //% weight=70
+	//% speed.min=0 speed.max=255
+	//% subcategory="执行器"
+    export function actuator_motor_run(INA: AnalogPin, INB: AnalogPin, turn: run_turn, speed: number): void {
+
+        if (turn == 0){
+			pins.analogWritePin(_INA,0)
+			pins.analogWritePin(_INB,speed)
+			
+		}else if (turn == 1){
+			pins.analogWritePin(_INA,speed)
+			pins.analogWritePin(_INB,0)
+		}
+
+    }
+		   	
+	/**
+	 * 激光模块
+	 */
+    //% blockId=actuator_laser block="actuator_laser pin ：%pin|status %status"   group="激光模块"
+	//% weight=70
+	//% subcategory="执行器"
+    export function actuator_laser(pin: DigitalPin,status: on_off): void {
+        pins.digitalWritePin(pin,status)
+    }
+	
+	/**
+	 * 红外发射
+	 */
+    export enum encodingType {
+        //% block="NEC"
+        NEC,
+        //% block="SONY"
+        SONY
+    }
+    let tempHandler: Action;
+    let irLed = AnalogPin.P16;
+    const pwmPeriod = 26;
+    pins.analogWritePin(irLed, 0);
+    pins.analogSetPeriod(irLed, pwmPeriod);
+    let send_init = false;
+    let rec_init = false;
+    let arr: number[] = []
+    let received = false
+    let first = true
+    let rec_Type = ""
+    let messageStr = ""
+    let recPin = DigitalPin.P8
+    let thereIsHandler = false
+    arr = []
+
+    function transmitBit(highTime: number, lowTime: number): void {
+        pins.analogWritePin(irLed, 512);
+        control.waitMicros(highTime);
+        pins.analogWritePin(irLed, 0);
+        control.waitMicros(lowTime);
+    }
+
+    /**
+     *  set the infrared LED pin.
+     */
+    //% blockId=setIR_pin block="set IR LED pin: %myPin" blockExternalInputs=false  group="红外发射"
+    //% weight=90 blockGap=10
+    //% myPin.fieldEditor="gridpicker" myPin.fieldOptions.columns=4
+	//% myPin.fieldOptions.tooltips="false" myPin.fieldOptions.width="300"
+	//% subcategory="执行器"
+    export function setIR_pin(myPin: AnalogPin) {
+        irLed = myPin;
+        pins.analogWritePin(irLed, 0);
+        pins.analogSetPeriod(irLed, pwmPeriod);
+        send_init = true;
+    }
+
+    /**
+     *  set the IR receiver pin.
+     */
+    //% blockId=setREC_pin block="set IR receiver pin: %myPin" blockExternalInputs=false  group="红外接收"
+    //% weight=85 blockGap=10
+    //% myPin.fieldEditor="gridpicker" myPin.fieldOptions.columns=4
+	//% myPin.fieldOptions.tooltips="false" myPin.fieldOptions.width="300"
+	//% subcategory="执行器"
+    export function setREC_pin(myPin: DigitalPin) {
+        recPin = myPin;
+        pins.setEvents(recPin, PinEventType.Pulse)
+        pins.setPull(recPin, PinPullMode.PullUp)
+        pins.onPulsed(recPin, PulseValue.Low, function () {
+            arr.push(input.runningTimeMicros())
+        })
+        pins.onPulsed(recPin, PulseValue.High, function () {
+            arr.push(input.runningTimeMicros())
+        })
+        control.onEvent(recPin, DAL.MICROBIT_PIN_EVENT_ON_TOUCH, tempHandler);
+        rec_init = true;
+    }
+
+    /**
+     * send message from IR LED. You must set the message encoding type, send how many times, and the message.
+     */
+    //% blockId=sendMyMessage1 block="send message: %msg| ,%times| times, encoding type:%myType"  group="红外发射"
+	//% weight=80 blockGap=10
+	//% subcategory="执行器"
+    export function sendMyMessage1(msg: string, times: number, myType: encodingType): void {
+        if (send_init) {
+            //control.inBackground(() => {
+            sendMessage(convertHexStrToNum(msg), times, myType);
+            //})
+        }
+    }
+    /**
+     * send message from IR LED. You must set the message encoding type, send how many times, and the message.
+     */
+    //% blockId=sendMyMessage2 block="send message: %msg| ,%times| times, encoding type:%myType"  group="红外发射"
+	//% weight=75 blockGap=10
+	//% subcategory="执行器"
+    export function sendMyMessage2(msg: string, times: number, myType: string): void {
+        if (send_init) {
+            if (myType == "NEC") {
+                sendMessage(convertHexStrToNum(msg), times, encodingType.NEC);
+            } else if (myType == "SONY") {
+                sendMessage(convertHexStrToNum(msg), times, encodingType.SONY);
+            }
+        }
+    }
+
+
+    function encode(myCode: number, bits: number, trueHigh: number, trueLow: number, falseHigh: number, falseLow: number): void {
+        const MESSAGE_BITS = bits;
+        for (let mask = 1 << (MESSAGE_BITS - 1); mask > 0; mask >>= 1) {
+            if (myCode & mask) {
+                transmitBit(trueHigh, trueLow);
+            } else {
+                transmitBit(falseHigh, falseLow);
+            }
+        }
+    }
+
+    function sendNEC(message: number, times: number): void {
+        const enum NEC {
+            startHigh = 9000,
+            startLow = 4500,
+            stopHigh = 560,
+            stopLow = 0,
+            trueHigh = 560,
+            trueLow = 1690,
+            falseHigh = 560,
+            falseLow = 560,
+            interval = 110000
+        }
+        let address = message >> 16;
+        let command = message % 0x010000;
+        const MESSAGE_BITS = 16;
+        let startTime = 0;
+        let betweenTime = 0;
+        for (let sendCount = 0; sendCount < times; sendCount++) {
+            startTime = input.runningTimeMicros();
+            transmitBit(NEC.startHigh, NEC.startLow);
+            encode(address, 16, NEC.trueHigh, NEC.trueLow, NEC.falseHigh, NEC.falseLow);
+            encode(command, 16, NEC.trueHigh, NEC.trueLow, NEC.falseHigh, NEC.falseLow);
+            transmitBit(NEC.stopHigh, NEC.stopLow);
+            betweenTime = input.runningTimeMicros() - startTime
+            if (times > 0)
+                control.waitMicros(NEC.interval - betweenTime);
+        }
+    }
+
+    function sendSONY(message: number, times: number): void {
+        const enum SONY {
+            startHigh = 2300,
+            startLow = 500,
+            trueHigh = 1100,
+            trueLow = 500,
+            falseHigh = 500,
+            falseLow = 500,
+            interval = 45000
+        }
+        const MESSAGE_BITS = 12;
+        let startTime = 0;
+        let betweenTime = 0;
+        for (let sendCount = 0; sendCount < times; sendCount++) {
+            startTime = input.runningTimeMicros();
+            transmitBit(SONY.startHigh, SONY.startLow);
+            encode(message, 12, SONY.trueHigh, SONY.trueLow, SONY.falseHigh, SONY.falseLow);
+            betweenTime = input.runningTimeMicros() - startTime
+            if (times > 0)
+                control.waitMicros(SONY.interval - betweenTime);
+        }
+    }
+
+    export function sendMessage(message: number, times: number, myType: encodingType): void {
+        switch (myType) {
+            case encodingType.NEC: sendNEC(message, times);
+            case encodingType.SONY: sendSONY(message, times);
+            default: sendNEC(message, times);
+        }
+    }
+
+    function convertHexStrToNum(myMsg: string): number {
+        let myNum = 0
+        for (let i = 0; i < myMsg.length; i++) {
+            if ((myMsg.charCodeAt(i) > 47) && (myMsg.charCodeAt(i) < 58)) {
+                myNum += (myMsg.charCodeAt(i) - 48) * (16 ** (myMsg.length - 1 - i))
+            } else if ((myMsg.charCodeAt(i) > 96) && (myMsg.charCodeAt(i) < 103)) {
+                myNum += (myMsg.charCodeAt(i) - 87) * (16 ** (myMsg.length - 1 - i))
+            } else if ((myMsg.charCodeAt(i) > 64) && (myMsg.charCodeAt(i) < 71)) {
+                myNum += (myMsg.charCodeAt(i) - 55) * (16 ** (myMsg.length - 1 - i))
+            } else {
+                myNum = 0
+                break
+            }
+        }
+        return myNum
+    }
+
+    //------------------receiver-------------
+
+    function resetReceiver() {
+        arr = []
+        received = false
+    }
+
+    control.inBackground(function () {
+        basic.forever(function () {
+            if ((!received) && (rec_init)) {
+                if (arr.length > 20) {
+                    if ((input.runningTimeMicros() - arr[arr.length - 1]) > 120000) {
+                        if (first) {
+                            resetReceiver()
+                            first = false
+                        } else {
+                            received = true
+                            decodeIR();
+                        }
+                    }
+                }
+            }
+        })
+    })
+
+    function decodeIR() {
+        let addr = 0
+        let command = 0
+        messageStr = ""
+        rec_Type = ""
+        for (let i = 0; i <= arr.length - 1 - 1; i++) {
+            arr[i] = arr[i + 1] - arr[i]
+        }
+        if (((arr[0] + arr[1]) > 13000) && ((arr[0] + arr[1]) < 14000)) {
+            rec_Type = "NEC"
+            arr.removeAt(1)
+            arr.removeAt(0)
+            addr = pulseToDigit(0, 15, 1600)
+            command = pulseToDigit(16, 31, 1600)
+            messageStr = convertNumToHexStr(addr, 4) + convertNumToHexStr(command, 4)
+            arr = [];
+            if (thereIsHandler) {
+                tempHandler();
+            }
+        } else if (((arr[0] + arr[1]) > 2600) && ((arr[0] + arr[1]) < 3200)) {
+            rec_Type = "SONY"
+            arr.removeAt(1)
+            arr.removeAt(0)
+            command = pulseToDigit(0, 11, 1300)
+            messageStr = convertNumToHexStr(command, 3)
+            arr = [];
+            if (thereIsHandler) {
+                tempHandler();
+            }
+        }
+        resetReceiver();
+    }
+
+    function pulseToDigit(beginBit: number, endBit: number, duration: number): number {
+        let myNum = 0
+        for (let i = beginBit; i <= endBit; i++) {
+            myNum <<= 1
+            if ((arr[i * 2] + arr[i * 2 + 1]) < duration) {
+                myNum += 0
+            } else {
+                myNum += 1
+            }
+        }
+        return myNum
+    }
+
+    function convertNumToHexStr(myNum: number, digits: number): string {
+        let tempDiv = 0
+        let tempMod = 0
+        let myStr = ""
+        tempDiv = myNum
+        while (tempDiv > 0) {
+            tempMod = tempDiv % 16
+            if (tempMod > 9) {
+                myStr = String.fromCharCode(tempMod - 10 + 97) + myStr
+            } else {
+                myStr = tempMod + myStr
+            }
+            tempDiv = Math.idiv(tempDiv, 16)
+        }
+        while (myStr.length != digits) {
+            myStr = "0" + myStr
+        }
+        return myStr
+    }
+
+    /**
+     * Do something when a receive IR
+     */
+    //% blockId=onReceivedIR block="on IR message received" blockInlineInputs=true  group="红外接收"
+	//% weight=70 blockGap=10
+	//% subcategory="执行器"
+    export function onReceivedIR(handler: Action): void {
+        tempHandler = handler
+        thereIsHandler = true
+    }
+
+    /**
+     * return the encoding type of the received IR 
+     */
+    //% blockId=getRecType block="the received IR encoding type"  group="红外接收"
+	//% weight=60 blockGap=10
+	//% subcategory="执行器"
+    export function getRecType(): string {
+        return rec_Type
+    }
+
+    /**
+     * return the message of the received IR 
+     */
+    //% blockId=getMessage block="the received IR message"  group="红外接收"
+	//% weight=60 blockGap=10
+	//% subcategory="执行器"
+    export function getMessage(): string {
+        return messageStr
+    }
+	
+	/**
+	 * 触摸键盘
+	 */
+
+    let _SDO=0
+    let _SCL=0
+
+    //% blockId=actuator_keyborad_pin block="actuator_keyborad_pin|SDOPIN %SDO|SCLPIN %SCL"   group="触摸键盘"
+	//% weight=71
+	//% subcategory="执行器"
+    export function actuator_keyborad_pin(SDO: DigitalPin, SCL: DigitalPin): void {
+        
+        _SDO=SDO
+        _SCL=SCL
+    }
+
+    //% blockId=actuator_keyborad_read block="actuator_keyborad_read"   group="触摸键盘"
+	//% weight=70
+	//% subcategory="执行器"
+    export function actuator_keyborad_read(): string {
+          let DATA = 0
+          pins.digitalWritePin(_SDO, 1)
+          control.waitMicros(93)
+
+          pins.digitalWritePin(_SDO, 0)
+          control.waitMicros(10)
+
+          for (let i = 0; i < 16; i++)
+          {
+               pins.digitalWritePin(_SCL, 1)
+               pins.digitalWritePin(_SCL, 0)
+               DATA |= pins.digitalReadPin(_SDO) << i
+          }
+          control.waitMicros(2 * 1000)
+          switch (DATA&0xFFFF)
+          {     
+               case 0xFFFE: return "1"
+               case 0xFFFD: return "2"
+               case 0xFFFB: return "3"
+               case 0xFFEF: return "4" 
+               case 0xFFDF: return "5"
+               case 0xFFBF: return "6"
+               case 0xFEFF: return "7"
+               case 0xFDFF: return "8"
+               case 0xFBFF: return "9"
+               case 0xDFFF: return "0"
+               case 0xFFF7: return "A"
+               case 0xFF7F: return "B"
+               case 0xF7FF: return "C"
+               case 0x7FFF: return "D"
+               case 0xEFFF: return "*"
+               case 0xBFFF: return "#"      
+               default: return " "
+          }          
+     }
 
 	//========================================华丽的分割线  显示器模块=============================================
 	
