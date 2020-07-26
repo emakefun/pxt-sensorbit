787c246dc26f8146f34a2f525f469cc86a45b0c2
joey@emakefun.com <joey@emakefun.com>
2020-07-26 17:46:17 +0800

    add tm1637

--- main.ts
+++ main.ts
@@ -191,7 +191,6 @@ namespace sensors {
         pins.digitalWritePin(a, _status)
     }
 
-
 	/**
 	 *  LCD 1602
 	 */	
@@ -199,7 +198,6 @@ namespace sensors {
 	let i2cAddr: number
     let BK: number
     let RS: number
-
     function setreg(d: number) {
         pins.i2cWriteNumber(i2cAddr, d, NumberFormat.Int8LE)
         basic.pause(1)
@@ -326,6 +324,253 @@ namespace sensors {
         lcdcmd(0)
     }
 	
+		
+	
+	
+		/**
+         * TM1637
+         */
+        let TM1637_CMD1 = 0x40
+        let TM1637_CMD2 = 0xC0
+        let TM1637_CMD3 = 0x80
+        let _SEGMENTS = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71]
+    
+        /**
+         * TM1637 LED display
+         */
+        export class TM1637LEDs {
+            buf: Buffer
+            clk: DigitalPin
+            dio: DigitalPin
+            _ON: number
+            brightness: number
+            count: number   // number of LEDs
+    
+            /**
+             * initial TM1637
+             */
+            init(): void {
+                pins.digitalWritePin(this.clk, 0)
+                pins.digitalWritePin(this.dio, 0)
+                this._ON = 8
+                this.buf = pins.createBuffer(this.count)
+                this.clear()
+            }
+    
+            /**
+             * Start 
+             */
+            _start() {
+                pins.digitalWritePin(this.dio, 0)
+                pins.digitalWritePin(this.clk, 0)
+            }
+    
+            /**
+             * Stop
+             */
+            _stop() {
+                pins.digitalWritePin(this.dio, 0)
+                pins.digitalWritePin(this.clk, 1)
+                pins.digitalWritePin(this.dio, 1)
+            }
+    
+            /**
+             * send command1
+             */
+            _write_data_cmd() {
+                this._start()
+                this._write_byte(TM1637_CMD1)
+                this._stop()
+            }
+    
+            /**
+             * send command3
+             */
+            _write_dsp_ctrl() {
+                this._start()
+                this._write_byte(TM1637_CMD3 | this._ON | this.brightness)
+                this._stop()
+            }
+    
+            /**
+             * send a byte to 2-wire interface
+             */
+            _write_byte(b: number) {
+                for (let i = 0; i < 8; i++) {
+                    pins.digitalWritePin(this.dio, (b >> i) & 1)
+                    pins.digitalWritePin(this.clk, 1)
+                    pins.digitalWritePin(this.clk, 0)
+                }
+                pins.digitalWritePin(this.clk, 1)
+                pins.digitalWritePin(this.clk, 0)
+            }
+    
+            /**
+             * set TM1637 intensity, range is [0-8], 0 is off.
+             * @param val the brightness of the TM1637, eg: 7
+             */
+            //% blockId="TM1637_set_intensity" block="%tm|set intensity %val"  group="TM1637数码管"
+            //% weight=50 blockGap=8
+			//% parts="TM1637"
+			//% subcategory="显示器"
+            intensity(val: number = 7) {
+                if (val < 1) {
+                    this.off()
+                    return
+                }
+                if (val > 8) val = 8 
+                this._ON = 8
+                this.brightness = val - 1
+                this._write_data_cmd()
+                this._write_dsp_ctrl()
+            }
+    
+            /**
+             * set data to TM1637, with given bit
+             */
+            _dat(bit: number, dat: number) {
+                this._write_data_cmd()
+                this._start()
+                this._write_byte(TM1637_CMD2 | (bit % this.count))
+                this._write_byte(dat)
+                this._stop()
+                this._write_dsp_ctrl()
+            }
+    
+            /**
+             * show a number in given position. 
+             * @param num number will show, eg: 5
+             * @param bit the position of the LED, eg: 0
+             */
+            //% blockId="TM1637_showbit" block="%tm|show digit %num |at %bit"  group="TM1637数码管"
+            //% weight=90 blockGap=8
+			//% parts="TM1637"
+			//% subcategory="显示器"
+            showbit(num: number = 5, bit: number = 0) {
+                this.buf[bit % this.count] = _SEGMENTS[num % 16]
+                this._dat(bit, _SEGMENTS[num % 16])
+            }
+    
+            /**
+              * show a number. 
+              * @param num is a number, eg: 0
+              */
+            //% blockId="TM1637_shownum" block="%tm|show number %num"  group="TM1637数码管"
+            //% weight=91 blockGap=8
+			//% parts="TM1637"
+			//% subcategory="显示器"
+            showNumber(num: number) {
+                if (num < 0) {
+                    this._dat(0, 0x40) // '-'
+                    num = -num
+                }
+                else
+                    this.showbit((num / 1000) % 10)
+                this.showbit(num % 10, 3)
+                this.showbit((num / 10) % 10, 2)
+                this.showbit((num / 100) % 10, 1)
+            }
+    
+            /**
+              * show a hex number. 
+              * @param num is a hex number, eg: 0
+              */
+            //% blockId="TM1637_showhex" block="%tm|show hex number %num"   group="TM1637数码管"
+            //% weight=90 blockGap=8
+			//% parts="TM1637"
+			//% subcategory="显示器"
+            showHex(num: number) {
+                if (num < 0) {
+                    this._dat(0, 0x40) // '-'
+                    num = -num
+                }
+                else
+                    this.showbit((num >> 12) % 16)
+                this.showbit(num % 16, 3)
+                this.showbit((num >> 4) % 16, 2)
+                this.showbit((num >> 8) % 16, 1)
+            }
+    
+            /**
+             * show or hide dot point. 
+             * @param bit is the position, eg: 1
+             * @param show is show/hide dp, eg: true
+             */
+            //% blockId="TM1637_showDP" block="%tm|DotPoint at %bit|show %status"  group="TM1637数码管"
+            //% weight=70 blockGap=8
+			//% parts="TM1637"
+			//% subcategory="显示器"
+            showDP(bit: number = 1, status: ledon_off) {
+				bit = bit % this.count
+				let show = status==1?true:false;
+                if (show) this._dat(bit, this.buf[bit] | 0x80)
+                else this._dat(bit, this.buf[bit] & 0x7F)
+            }
+    
+            /**
+             * clear LED. 
+             */
+            //% blockId="TM1637_clear" block="clear %tm"  group="TM1637数码管"
+            //% weight=80 blockGap=8
+			//% parts="TM1637"
+			//% subcategory="显示器"
+            clear() {
+                for (let i = 0; i < this.count; i++) {
+                    this._dat(i, 0)
+                    this.buf[i] = 0
+                }
+            }
+    
+            /**
+             * turn on LED. 
+             */
+            //% blockId="TM1637_on" block="turn on %tm"  group="TM1637数码管"
+            //% weight=86 blockGap=8
+			//% parts="TM1637"
+			//% subcategory="显示器"
+            on() {
+                this._ON = 8
+                this._write_data_cmd()
+                this._write_dsp_ctrl()
+            }
+    
+            /**
+             * turn off LED. 
+             */
+            //% blockId="TM1637_off" block="turn off %tm"  group="TM1637数码管"
+            //% weight=85 blockGap=8
+			//% parts="TM1637"
+			//% subcategory="显示器"
+            off() {
+                this._ON = 0
+                this._write_data_cmd()
+                this._write_dsp_ctrl()
+            }
+        }
+    
+        /**
+         * create a TM1637 object.
+         * @param clk the CLK pin for TM1637, eg: DigitalPin.P1
+         * @param dio the DIO pin for TM1637, eg: DigitalPin.P2
+         * @param intensity the brightness of the LED, eg: 7
+         * @param count the count of the LED, eg: 4
+         */
+        //% weight=200 blockGap=8
+		//% blockId="TM1637_create" block="CLK %clk|DIO %dio|intensity %intensity|LED count %count"  group="TM1637数码管"
+		//% subcategory="显示器"
+        export function TMcreate(clk: DigitalPin, dio: DigitalPin, intensity: number, count: number): TM1637LEDs {
+            let tm = new TM1637LEDs()
+            tm.clk = clk
+            tm.dio = dio
+            if ((count < 1) || (count > 5)) count = 4
+            tm.count = count
+            tm.brightness = intensity
+            tm.init()
+            return tm
+        }
+
+
+
 
 	
 
