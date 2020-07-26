aad88272c0031d13808bdac4658bb9c54e3eaf92
joey@emakefun.com <joey@emakefun.com>
2020-07-26 18:19:11 +0800

    add TM1650

--- main.ts
+++ main.ts
@@ -409,7 +409,7 @@ namespace sensors {
              * set TM1637 intensity, range is [0-8], 0 is off.
              * @param val the brightness of the TM1637, eg: 7
              */
-            //% blockId="TM1637_set_intensity" block="%tm|set intensity %val"  group="TM1637数码管"
+            //% blockId="TM1637_set_intensity" block="set intensity %val"  group="TM1637数码管"
             //% weight=88 blockGap=8
 			//% parts="TM1637"
 			//% subcategory="显示器"
@@ -442,7 +442,7 @@ namespace sensors {
              * @param num number will show, eg: 5
              * @param bit the position of the LED, eg: 0
              */
-            //% blockId="TM1637_showbit" block="%tm|show digit %num |at %bit"  group="TM1637数码管"
+            //% blockId="TM1637_showbit" block="show digit %num |at %bit"  group="TM1637数码管"
             //% weight=90 blockGap=8
 			//% parts="TM1637"
 			//% subcategory="显示器"
@@ -455,7 +455,7 @@ namespace sensors {
               * show a number. 
               * @param num is a number, eg: 0
               */
-            //% blockId="TM1637_shownum" block="%tm|show number %num"  group="TM1637数码管"
+            //% blockId="TM1637_shownum" block="show number %num"  group="TM1637数码管"
             //% weight=91 blockGap=8
 			//% parts="TM1637"
 			//% subcategory="显示器"
@@ -475,7 +475,7 @@ namespace sensors {
               * show a hex number. 
               * @param num is a hex number, eg: 0
               */
-            //% blockId="TM1637_showhex" block="%tm|show hex number %num"   group="TM1637数码管"
+            //% blockId="TM1637_showhex" block="show hex number %num"   group="TM1637数码管"
             //% weight=90 blockGap=8
 			//% parts="TM1637"
 			//% subcategory="显示器"
@@ -496,7 +496,7 @@ namespace sensors {
              * @param bit is the position, eg: 1
              * @param show is show/hide dp, eg: true
              */
-            //% blockId="TM1637_showDP" block="%tm|DotPoint at %bit|show %status"  group="TM1637数码管"
+            //% blockId="TM1637_showDP" block="DotPoint at %bit|show %status"  group="TM1637数码管"
             //% weight=70 blockGap=8
 			//% parts="TM1637"
 			//% subcategory="显示器"
@@ -510,7 +510,7 @@ namespace sensors {
             /**
              * clear LED. 
              */
-            //% blockId="TM1637_clear" block="clear %tm"  group="TM1637数码管"
+            //% blockId="TM1637_clear" block="clear"  group="TM1637数码管"
             //% weight=80 blockGap=8
 			//% parts="TM1637"
 			//% subcategory="显示器"
@@ -524,7 +524,7 @@ namespace sensors {
             /**
              * turn on LED. 
              */
-            //% blockId="TM1637_on" block="turn on %tm"  group="TM1637数码管"
+            //% blockId="TM1637_on" block="turn on"  group="TM1637数码管"
             //% weight=86 blockGap=8
 			//% parts="TM1637"
 			//% subcategory="显示器"
@@ -537,7 +537,7 @@ namespace sensors {
             /**
              * turn off LED. 
              */
-            //% blockId="TM1637_off" block="turn off %tm"  group="TM1637数码管"
+            //% blockId="TM1637_off" block="turn off"  group="TM1637数码管"
             //% weight=85 blockGap=8
 			//% parts="TM1637"
 			//% subcategory="显示器"
@@ -571,7 +571,142 @@ namespace sensors {
         }
 
 
-
+		/**
+         * TM1650
+         */
+        let COMMAND_I2C_ADDRESS = 0x24
+        let DISPLAY_I2C_ADDRESS = 0x34
+        let _SEG = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71] 
+    
+        let _intensity = 3
+        let dbuf = [0, 0, 0, 0]
+    
+        /**
+         * send command to display
+         * @param is command, eg: 0
+         */
+        function cmd(c: number) {
+            pins.i2cWriteNumber(COMMAND_I2C_ADDRESS, c, NumberFormat.Int8BE)
+        }
+    
+        /**
+         * send data to display
+         * @param is data, eg: 0
+         */
+        function dat(bit: number, d: number) {
+            pins.i2cWriteNumber(DISPLAY_I2C_ADDRESS + (bit % 4), d, NumberFormat.Int8BE)
+        }
+    
+        /**
+         * turn on display
+         */
+        //% blockId="TM650_ON" block="turn on display" group="TM1650数码管"
+        //% weight=50 blockGap=8
+        export function on() {
+            cmd(_intensity * 16 + 1)
+        }
+    
+        /**
+         * turn off display
+         */
+        //% blockId="TM650_OFF" block="turn off display" group="TM1650数码管"
+        //% weight=50 blockGap=8
+        export function off() {
+            _intensity = 0
+            cmd(0)
+        }
+    
+        /**
+         * clear display content
+         */
+        //% blockId="TM650_CLEAR" block="clear display" group="TM1650数码管"
+        //% weight=40 blockGap=8
+        export function clear() {
+            dat(0, 0)
+            dat(1, 0)
+            dat(2, 0)
+            dat(3, 0)
+            dbuf = [0, 0, 0, 0]
+        }
+    
+        /**
+         * show a digital in given position
+         * @param digit is number (0-15) will be shown, eg: 1
+         * @param bit is position, eg: 0
+         */
+        //% blockId="TM650_DIGIT" block="show digit %num|at %bit"  group="TM1650数码管"
+        //% weight=80 blockGap=8
+        //% num.max=15 num.min=0
+        export function digit(bit: number, num: number) {
+            dbuf[bit % 4] = _SEG[num % 16]
+            dat(bit, _SEG[num % 16])
+        }
+    
+        /**
+         * show a number in display
+         * @param num is number will be shown, eg: 100
+         */
+        //% blockId="TM650_SHOW_NUMBER" block="show number %num"  group="TM1650数码管"
+        //% weight=100 blockGap=8
+        export function showNumber(num: number) {
+            if (num < 0) {
+                dat(0, 0x40) // '-'
+                num = -num
+            }
+            else
+                digit(Math.idiv(num, 1000) % 10, 0)
+            digit(num % 10, 3)
+            digit(Math.idiv(num, 10) % 10, 2)
+            digit(Math.idiv(num, 100) % 10, 1)
+        }
+    
+        /**
+         * show a number in hex format
+         * @param num is number will be shown, eg: 123
+         */
+        //% blockId="TM650_SHOW_HEX_NUMBER" block="show hex number %num"  group="TM1650数码管"
+        //% weight=90 blockGap=8
+        export function showHex(num: number) {
+            if (num < 0) {
+                dat(0, 0x40) // '-'
+                num = -num
+            }
+            else
+                digit((num >> 12) % 16, 0)
+            digit(num % 16, 3)
+            digit((num >> 4) % 16, 2)
+            digit((num >> 8) % 16, 1)
+        }
+    
+        /**
+         * show Dot Point in given position
+         * @param bit is positiion, eg: 0
+         * @param show is true/false, eg: true
+         */
+        //% blockId="TM650_SHOW_DP" block="show dot point %bit|show %num" group="TM1650数码管"
+        //% weight=80 blockGap=8
+        export function showDpAt(bit: number, status: ledon_off) {
+			let show = status==1?true:false;
+            if (show) dat(bit, dbuf[bit % 4] | 0x80)
+            else dat(bit, dbuf[bit % 4] & 0x7F)
+        }
+    
+        /**
+         * set display intensity
+         * @param dat is intensity of the display, eg: 3
+         */
+        //% blockId="TM650_INTENSITY" block="set intensity %dat" group="TM1650数码管"
+        //% weight=70 blockGap=8
+        export function setIntensity(dat: number) {
+            if ((dat < 0) || (dat > 8))
+                return 
+            if (dat == 0)
+                off()
+            else {
+                _intensity = dat
+                cmd((dat << 4) | 0x01)
+            }
+        }
 
 	
 
