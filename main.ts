5cd02f15b87eb627f5c06dbb6219fa64660e3700
emakefun <33374788+emakefun@users.noreply.github.com>
2021-06-15 14:36:21 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -74,28 +74,6 @@ enum waterpin {
     P10,
 }
 
-enum IrPins {
-    P0 = 3,
-    P1 = 2,
-    P2 = 1,
-    P3 = 4,
-    P4 = 5,
-    P5 = 17,
-    P6 = 12,
-    P7 = 11,
-    P8 = 18,
-    P9 = 10,
-    P10 = 6,
-    P11 = 26,
-    P12 = 20,
-    P13 = 23,
-    P14 = 22,
-    P15 = 21,
-    P16 = 16,
-    P19 = 0,
-    P20 = 30,
-}
-
 enum _rockerpin {
     //% block="Xpin"
     Xpin = 0,
