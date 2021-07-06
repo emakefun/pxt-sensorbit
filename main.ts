6f851ba91573d3a990f62939d6600e996b170197
Buyuliang <63707976+Buyuliang@users.noreply.github.com>
2021-07-06 16:31:07 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2012,6 +2012,276 @@ namespace sensors {
 //    let _buf3 = pins.createBuffer(3);
 //    let _buf4 = pins.createBuffer(4);
 //    let _ZOOM = 1;
-//}    
+//
+//
+//    function cmd1(d: number) {
+//        let n = d % 256;
+//        pins.i2cWriteNumber(_I2CAddr, n, NumberFormat.UInt16BE);
+//    }
+//
+//    function cmd2(d1: number, d2: number) {
+//        _buf3[0] = 0;
+//        _buf3[1] = d1;
+//        _buf3[2] = d2;
+//        pins.i2cWriteBuffer(_I2CAddr, _buf3);
+//    }
+//
+//    function cmd3(d1: number, d2: number, d3: number) {
+//        _buf4[0] = 0;
+//        _buf4[1] = d1;
+//        _buf4[2] = d2;
+//        _buf4[3] = d3;
+//        pins.i2cWriteBuffer(_I2CAddr, _buf4);
+//    }
+//
+//    function set_pos(col: number = 0, page: number = 0) {
+//        cmd1(0xb0 | page) // page number
+//        let c = col * (_ZOOM + 1)
+//        cmd1(0x00 | (c % 16)) // lower start column address
+//        cmd1(0x10 | (c >> 4)) // upper start column address    
+//    }
+//
+//    // clear bit
+//    function clrbit(d: number, b: number): number {
+//        if (d & (1 << b))
+//            d -= (1 << b)
+//        return d
+//    }
+//    
+//    /**
+//     * 在 OLED 上显示一个像素
+//     * @param x is X alis, eg: 0
+//     * @param y is Y alis, eg: 0
+//     */
+//  //% blockId=OLED12864_I2C_PIXEL block="Display pixels x %x|y %y" group="OLED"
+//  //% subcategory="显示器"
+//  //% weight=70 blockGap=8
+//
+//    export function OLED12864_I2C_PIXEL(x: number, y: number) {
+//        let page = y >> 3
+//        let shift_page = y % 8
+//        let ind = x * (_ZOOM + 1) + page * 128 + 1
+//        let b = (color) ? (_screen[ind] | (1 << shift_page)) : clrbit(_screen[ind], shift_page)
+//        _screen[ind] = b
+//        set_pos(x, page)
+//        if (_ZOOM) {
+//            _screen[ind + 1] = b
+//            _buf3[0] = 0x40
+//            _buf3[1] = _buf3[2] = b
+//            pins.i2cWriteBuffer(_I2CAddr, _buf3)
+//        }
+//        else {
+//            _buf2[0] = 0x40
+//            _buf2[1] = b
+//            pins.i2cWriteBuffer(_I2CAddr, _buf2)
+//        }
+//    }
+//
+//    /**
+//     * 显示一个字符串
+//     * @param x is X alis, eg: 0
+//     * @param y is Y alis, eg: 0
+//     * @param s is the text will be show, eg: 'Hello!'
+//     */
+//      //% blockId=OLED12864_I2C_SHOWSTRING block="Display text x %x|y %y|text %s" group="OLED"
+//      //% subcategory="显示器"
+//      //% weight=80 blockGap=8
+//
+//    export function OLED12864_I2C_SHOWSTRING(x: number, y: number, s: string) {
+//        let col = 0
+//        let p = 0
+//        let ind = 0
+//        for (let n = 0; n < s.length; n++) {
+//            p = font[s.charCodeAt(n)]
+//            for (let i = 0; i < 5; i++) {
+//                col = 0
+//                for (let j = 0; j < 5; j++) {
+//                    if (p & (1 << (5 * i + j)))
+//                        col |= (1 << (j + 1))
+//                }
+//                ind = (x + n) * 5 * (_ZOOM + 1) + y * 128 + i * (_ZOOM + 1) + 1
+//                if (color == 0)
+//                    col = 255 - col
+//                _screen[ind] = col
+//                if (_ZOOM)
+//                    _screen[ind + 1] = col
+//            }
+//        }
+//        set_pos(x * 5, y)
+//        let ind0 = x * 5 * (_ZOOM + 1) + y * 128
+//        let buf = _screen.slice(ind0, ind + 1)
+//        buf[0] = 0x40
+//        pins.i2cWriteBuffer(_I2CAddr, buf)
+//    }
+//
+//    /**
+//     * 显示一个整数
+//     * @param x is X alis, eg: 0
+//     * @param y is Y alis, eg: 0
+//     * @param num is the number will be show, eg: 12
+//     */
+//    //% blockId=OLED12864_I2C_NUMBER block="Display number x %x|y %y|number %num" group="OLED"
+//    //% subcategory="显示器"
+//    //% weight=80 blockGap=8
+//
+//    export function OLED12864_I2C_NUMBER(x: number, y: number, num: number) {
+//        OLED12864_I2C_SHOWSTRING(x, y, num.toString())
+//    }
+//
+//    /**
+//     * 绘制一条水平线段
+//     * @param x is X alis, eg: 0s
+//     * @param y is Y alis, eg: 0
+//     * @param len is the length of line, eg: 10
+//     */
+//    //% blockId=OLED12864_I2C_HLINE block="Draw horizontal line segment x %x|y %y|length %len" group="OLED"
+//    //% subcategory="显示器"
+//    //% weight=71 blockGap=8
+//    //% x.min=0 x.max=60
+//    //% y.min=0 y.max=30
+//    //% len.min=1 len.max=62
+//
+//    export function OLED12864_I2C_HLINE(x: number, y: number, len: number) {
+//        for (let i = x; i < (x + len); i++){
+//            OLED12864_I2C_PIXEL(i, y)
+//            basic.pause(1)
+//        }
+//    }
+//
+//    /**
+//     * 绘制一条垂直线段
+//     * @param x is X alis, eg: 0
+//     * @param y is Y alis, eg: 0
+//     * @param len is the length of line, eg: 10
+//     */
+//    //% blockId=OLED12864_I2C_VLINE block="Draw vertical line segments x %x|y %y|length %len" group="OLED"
+//    //% subcategory="显示器"
+//    //% weight=72 blockGap=8
+//    //% x.min=0 x.max=60
+//    //% y.min=0 y.max=30
+//    //% len.min=1 len.max=30
+//
+//    export function OLED12864_I2C_VLINE(x: number, y: number, len: number) {
+//        for (let i = y; i < (y + len); i++){
+//            OLED12864_I2C_PIXEL(x, i)
+//            basic.pause(1)
+//        }
+//            
+//
+//    }
+//
+//    /**
+//     * 在指定位置绘制矩形
+//     * @param x1 is X alis, eg: 0
+//     * @param y1 is Y alis, eg: 0
+//     * @param x2 is X alis, eg: 60
+//     * @param y2 is Y alis, eg: 30
+//     */
+//    //% blockId=OLED12864_I2C_RECT block="Draw rectangle x1 %x1|y1 %y1|x2 %x2|y2 %y2" group="OLED"
+//    //% subcategory="显示器"
+//    //% weight=73 blockGap=8
+//    //% x1.min=0 x1.max=60
+//    //% y1.min=0 y1.max=30
+//    //% x2.min=0 x2.max=60
+//    //% y2.min=0 y2.max=30
+//
+//    export function OLED12864_I2C_RECT(x1: number, y1: number, x2: number, y2: number) {
+//        if (x1 > x2){
+//            x1 = [x2, x2 = x1][0];
+//        }
+//            
+//        if (y1 > y2){
+//            y1 = [y2, y2 = y1][0];
+//        }
+//        OLED12864_I2C_HLINE(x1, y1, x2 - x1 + 1)
+//        OLED12864_I2C_VLINE(x2, y1, y2 - y1 + 1)
+//        OLED12864_I2C_HLINE(x1, y2, x2 - x1 + 1)
+//        OLED12864_I2C_VLINE(x1, y1, y2 - y1 + 1)
+//    }
+//
+//
+//    // /**
+//    //  * 重新绘制屏幕的显示内容
+//    //  */
+//    // //% blockId="OLED12864_I2C_DRAW" block="刷新显示" group="OLED"
+//    //% subcategory="显示器"
+//    // //% weight=64 blockGap=8
+//    // export 
+//
+//    function draw() {
+//        set_pos()
+//        pins.i2cWriteBuffer(_I2CAddr, _screen)
+//    }
+//
+//    /**
+//     * 清除 OLED 模块的显示内容
+//     */
+//    //% blockId=OLED12864_I2C_CLEAR block="Clear display" group="OLED"
+//    //% subcategory="显示器"
+//    //% weight=63 blockGap=8
+//    export function OLED12864_I2C_CLEAR() {
+//        OLED12864_I2C_init()
+//    }
+//
+//
+//    function clear() {
+//        _screen.fill(0)
+//        _screen[0] = 0x40
+//        draw()
+//    }
+//
+//    /**
+//     * 打开 OLED 模块的屏幕显示
+//     */
+//    //% blockId=OLED12864_I2C_ON block="Show on" group="OLED"
+//    //% subcategory="显示器"
+//    //% weight=62 blockGap=8
+//    export function OLED12864_I2C_ON() {
+//        cmd1(0xAF)
+//    }
+//
+//    /**
+//     * 关闭 OLED 模块的屏幕显示
+//     */
+//    //% blockId=OLED12864_I2C_OFF block="Display off" group="OLED"
+//    //% subcategory="显示器"
+//    //% weight=61 blockGap=8
+//    export function OLED12864_I2C_OFF() {
+//        cmd1(0xAE)
+//    }
+//
+//
+//    /**
+//     * OLED 初始化
+//     */
+//    //% blockId=OLED12864_I2C_init block="Initialize OLED" group="OLED"
+//    //% subcategory="显示器"
+//    //% weight=100 blockGap=8
+//    export function OLED12864_I2C_init() {
+//        cmd1(0xAE)       // SSD1306_DISPLAYOFF
+//        cmd1(0xA4)       // SSD1306_DISPLAYALLON_RESUME
+//        cmd2(0xD5, 0xF0) // SSD1306_SETDISPLAYCLOCKDIV
+//        cmd2(0xA8, 0x3F) // SSD1306_SETMULTIPLEX
+//        cmd2(0xD3, 0x00) // SSD1306_SETDISPLAYOFFSET
+//        cmd1(0 | 0x0)    // line #SSD1306_SETSTARTLINE
+//        cmd2(0x8D, 0x14) // SSD1306_CHARGEPUMP
+//        cmd2(0x20, 0x00) // SSD1306_MEMORYMODE
+//        cmd3(0x21, 0, 127) // SSD1306_COLUMNADDR
+//        cmd3(0x22, 0, 63)  // SSD1306_PAGEADDR
+//        cmd1(0xa0 | 0x1) // SSD1306_SEGREMAP
+//        cmd1(0xc8)       // SSD1306_COMSCANDEC
+//        cmd2(0xDA, 0x12) // SSD1306_SETCOMPINS
+//        cmd2(0x81, 0xCF) // SSD1306_SETCONTRAST
+//        cmd2(0xd9, 0xF1) // SSD1306_SETPRECHARGE
+//        cmd2(0xDB, 0x40) // SSD1306_SETVCOMDETECT
+//        cmd1(0xA6)       // SSD1306_NORMALDISPLAY
+//        cmd2(0xD6, 1)    // zoom on
+//        cmd1(0xAF)       // SSD1306_DISPLAYON
+//        clear()
+//        _ZOOM = 1
+//    }
+//    
+//
+    
 
 }
